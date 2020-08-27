4829de3b5b23b39924bca3c8edf553aa34721899
SajanKc <sajan1996kc@gmail.com>
2020-08-27 21:38:50 +0545

    Updates

---
+++ precache-manifest.a3547d3aef04c622be2276019a805bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10d26be83b9405f369d5bbf31d0198b",
+    "url": "/bca-mcq/index.html"
+  },
+  {
+    "revision": "67469447854ac3526a43",
+    "url": "/bca-mcq/static/css/main.951da8b4.chunk.css"
+  },
+  {
+    "revision": "71a2ab97a6258c97f967",
+    "url": "/bca-mcq/static/js/2.200a21ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bca-mcq/static/js/2.200a21ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67469447854ac3526a43",
+    "url": "/bca-mcq/static/js/main.4a209eff.chunk.js"
+  },
+  {
+    "revision": "70724d254b23bd3e5165",
+    "url": "/bca-mcq/static/js/runtime-main.9f9c1e19.js"
+  }
+]);
